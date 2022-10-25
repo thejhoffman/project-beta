4fa477c71cc8729465562d5d1705da2e96e3a305
James Hoffman <jamesvhoffman@outlook.com>
2022-10-25 17:38:53 -0400

    Completed form for adding a new customer

--- ghi/app/src/Sales/CustomerForm.js
+++ ghi/app/src/Sales/CustomerForm.js
@@ -1,6 +1,93 @@
+import React, { useState } from 'react';
+
 const CustomerForm = (props) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone_number: ""
+  });
+
+  const handleFormData = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { ...formData };
+    const url = 'http://localhost:8090/api/sales/customers/';
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      setFormData({
+        name: "",
+        address: "",
+        phone_number: ""
+      });
+    }
+  };
+
   return (
-    <div>CustomerForm</div>
+    <div className=" container mt-2">
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Add new customer</h1>
+            <form onSubmit={handleSubmit} id="add-customer-form">
+              <div className="form-floating mb-3">
+                <input
+                  onChange={handleFormData}
+                  value={formData.name}
+                  className="form-control"
+                  placeholder="name"
+                  required
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="form-floating mb-3">
+                <textarea
+                  onChange={handleFormData}
+                  value={formData.address}
+                  className="form-control"
+                  placeholder="address"
+                  required
+                  type="text"
+                  id="address"
+                  name="address"
+                  cols="30"
+                  rows="4"
+                  style={{ height: "100%" }}
+                />
+                <label htmlFor="address">Address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  onChange={handleFormData}
+                  value={formData.phone_number}
+                  className="form-control"
+                  placeholder="phone_number"
+                  required
+                  type="text"
+                  id="phone_number"
+                  name="phone_number"
+                />
+                <label htmlFor="phone_number">Phone number</label>
+              </div>
+              <button className="btn btn-primary">Add</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
