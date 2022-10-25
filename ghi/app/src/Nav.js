ef53e34fda92d7f36fd274e51e8fba551ecb8dd6
James Hoffman <jamesvhoffman@outlook.com>
2022-10-25 11:18:04 -0400

    Updated navbar structure

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,11 +10,52 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <div className="nav-link dropdown-toggle" id="navbarInventoryDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Inventory
+              </div>
+              <div className="dropdown-menu" aria-labelledby="navbarInventoryDropdown">
+                <NavLink className="dropdown-item" to="/manufacturers">Manufacturers list</NavLink>
+                <NavLink className="dropdown-item" to="/manufacturers/new">Add new manufacturer</NavLink>
+                <div className="dropdown-divider"></div>
+                <NavLink className="dropdown-item" to="/vehicle_models">Vehicle models List</NavLink>
+                <NavLink className="dropdown-item" to="/vehicle_models/new">Add new vehicle model</NavLink>
+                <div className="dropdown-divider"></div>
+                <NavLink className="dropdown-item" to="/automobiles">Automobiles list</NavLink>
+                <NavLink className="dropdown-item" to="/automobiles/new">Add new automobile</NavLink>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <div className="nav-link dropdown-toggle" id="navbarServiceDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Service
+              </div>
+              <div className="dropdown-menu" aria-labelledby="navbarServiceDropdown">
+                <NavLink className="dropdown-item" to="/path_goes_here">All appointments</NavLink>
+                <NavLink className="dropdown-item" to="/path_goes_here">Service history</NavLink>
+                <div className="dropdown-divider"></div>
+                <NavLink className="dropdown-item" to="/path_goes_here">New service appointment</NavLink>
+                <NavLink className="dropdown-item" to="/path_goes_here">New technician</NavLink>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <div className="nav-link dropdown-toggle" id="navbarSalesDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sales
+              </div>
+              <div className="dropdown-menu" aria-labelledby="navbarSalesDropdown">
+                <NavLink className="dropdown-item" to="/records">All sale records</NavLink>
+                <NavLink className="dropdown-item" to="/records/history">Sale history</NavLink>
+                <div className="dropdown-divider"></div>
+                <NavLink className="dropdown-item" to="/customers/new">New customer</NavLink>
+                <NavLink className="dropdown-item" to="/staff/new">New sales person</NavLink>
+                <div className="dropdown-divider"></div>
+                <NavLink className="dropdown-item" to="/records/new">Create sales record</NavLink>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav;
