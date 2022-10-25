e1fb073b1709a18c1e712811273d83e9decf8a01
James Hoffman <jamesvhoffman@outlook.com>
2022-10-25 14:23:42 -0400

    Completed form for adding a manufacturer

--- ghi/app/src/Inventory/ManufacturerForm.js
+++ ghi/app/src/Inventory/ManufacturerForm.js
@@ -1,6 +1,56 @@
+import React, { useState } from 'react';
+
 const ManufacturerForm = (props) => {
+  const [formData, setFormData] = useState({ name: "" });
+
+  const handleFormData = (event) => {
+    setFormData({ [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = { ...formData };
+    const url = 'http://localhost:8100/api/manufacturers/';
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      setFormData({ name: "" });
+    }
+  };
+
   return (
-    <div>ManufacturerForm</div>
+    <div className=" container mt-2">
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Create a manufacturer</h1>
+            <form onSubmit={handleSubmit} id="add-hat-form">
+              <div className="form-floating mb-3">
+                <input
+                  onChange={handleFormData}
+                  value={formData.name}
+                  className="form-control"
+                  placeholder="name"
+                  required
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <button className="btn btn-primary">Add</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
