f5d84fcc16ad75e10c000eea69e186769cc5c904
James Hoffman <jamesvhoffman@outlook.com>
2022-10-25 13:40:29 -0400

    Completed component for manufacturers list

--- ghi/app/src/Inventory/ManufacturerList.js
+++ ghi/app/src/Inventory/ManufacturerList.js
@@ -1,6 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
 const ManufacturerList = (props) => {
+  const [manufacturers, setManufacturers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:8100/api/manufacturers/');
+      if (response.ok) {
+        const data = await response.json();
+        setManufacturers(data.manufacturers);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>ManufacturerList</div>
+    <div className="container mt-2">
+      <h1>Manufacturers</h1>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {manufacturers.map(manufacturer => {
+            return (
+              <tr>
+                <td>{manufacturer.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
