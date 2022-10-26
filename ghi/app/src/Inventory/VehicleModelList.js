6a0ad08dc80fa8c73a84efcb11066d380a8cae36
James Hoffman <jamesvhoffman@outlook.com>
2022-10-26 12:04:14 -0400

    Completed component for listing all vehicle models

--- ghi/app/src/Inventory/VehicleModelList.js
+++ ghi/app/src/Inventory/VehicleModelList.js
@@ -1,6 +1,47 @@
+import React, { useState, useEffect } from 'react';
+
 const VehicleModelList = (props) => {
+  const [vehicleModels, setVehicleModels] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:8100/api/models/');
+      if (response.ok) {
+        const data = await response.json();
+        setVehicleModels(data.models);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const addDefaultSrc = (event) => {
+    event.target.src = 'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg';
+  };
+
   return (
-    <div>VehicleModelList</div>
+    <div className="container mt-2">
+      <h1>Vehicle models</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Manufacturer</th>
+            <th>Picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vehicleModels.map((model, index) => {
+            return (
+              <tr key={index}>
+                <td>{model.name}</td>
+                <td>{model.manufacturer.name}</td>
+                <td><img onError={addDefaultSrc} src={model.picture_url} width="200" alt="car" /></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
