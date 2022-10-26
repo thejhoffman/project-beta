3ce9c06f00773dbc9e027a487c0ba3d9ec0e4e0a
James Hoffman <jamesvhoffman@outlook.com>
2022-10-26 10:30:18 -0400

    Completed component for listing all sales records

--- ghi/app/src/Sales/RecordList.js
+++ ghi/app/src/Sales/RecordList.js
@@ -1,6 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
 const RecordList = (props) => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:8090/api/sales/records/');
+      if (response.ok) {
+        const data = await response.json();
+        setRecords(data.records);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>RecordList</div>
+    <div className="container mt-2">
+      <h1>Sales records</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Sales person</th>
+            <th>Customer</th>
+            <th>VIN</th>
+            <th>Sale price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((record, index) => {
+            return (
+              <tr key={index}>
+                <td>{record.sales_person}</td>
+                <td>{record.customer}</td>
+                <td>{record.vin}</td>
+                <td>{record.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
