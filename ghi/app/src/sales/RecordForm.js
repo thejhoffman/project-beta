f94b7a7ab7a1f38482ec982c018da8c79fe868f2
James Hoffman <jamesvhoffman@outlook.com>
2022-11-02 16:21:25 -0400

    Tweak to imports

--- ghi/app/src/sales/RecordForm.js
+++ ghi/app/src/sales/RecordForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useFetch from '../hooks/useFetch';
 import usePostForm from '../hooks/usePostForm';
 
